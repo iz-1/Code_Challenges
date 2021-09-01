6d7809fabab20dc85f9c18581c3fe736885a7d2e
unknown <gtsmang@gmail.com>
2021-09-01 10:14:24 -0700

    move logical challenge

--- 210830_SortedArray2BinTree/src/com/Main.java
+++ 210830_SortedArray2BinTree/src/com/Main.java
@@ -16,24 +16,3 @@ public class Main {
         btree.printLevel();
     }
 }
-
-//        Logical Reasoning:
-//        A watch, which loses time uniformly, was observed to be 5 minutes fast at 8.00 p.m. on Thursday.
-//        It was noticed to be 7 minutes slow at 8.00 a.m. on the subsequent Monday.
-//        When did the watch show the correct time ? Explanation needed.
-
-//        a) 7 a.m. Saturday
-//        b) 7 a.m. on Friday
-//        c) 10a.m. on Sunday
-//        d) 11 a.m. on Friday
-
-//    a)
-//  from thurs 8pm to monday 8am is 84 hours
-//  find equation for the difference
-//  f(x) = kx + 5  where f(0) = 5, f(84) = -7
-//  f(84) solve for k
-//  f(x) = -1/7 * x + 5
-//  find when clock is on time (0)
-//  0 = -1/7 * x + 5, solve for x
-//  35 = x
-//  35hrs from thurs 8pm is sat 7am
